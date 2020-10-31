69250824210ef4b38283afd82475de7e0c47ce1f
Saurav <Saurav.KC14@myhunter.cuny.edu>
2020-10-31 17:27:50 -0400

    Updated file and names

--- Tcomo Ext/background.js
+++ Tcomo Ext/background.js
@@ -6,12 +6,12 @@
 
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+    console.log("The extension is activated!");
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
+        pageUrl: {urlContains: '.com'},
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
